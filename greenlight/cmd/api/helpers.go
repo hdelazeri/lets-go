c16776852e7c5bb8972bb9493618423cf52df98a
Henrique Delazeri <hdelazeri@hdelazeri.dev>
2026-03-02 18:37:28 -0300

    [LGF] Chapter 4.3: Restricting inputs

--- greenlight/cmd/api/helpers.go
+++ greenlight/cmd/api/helpers.go
@@ -46,7 +46,12 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshallTypeError *json.UnmarshalTypeError
@@ -75,6 +80,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		default:
 			return err
 		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
