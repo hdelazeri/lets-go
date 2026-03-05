dbcc7db4de2bc50d0fa2c5ee6575193df6880f06
Henrique Delazeri <hdelazeri@hdelazeri.dev>
2026-03-05 19:39:57 -0300

    [LGF] Chapter 19.6: Managing and automating version numbers

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -14,10 +15,11 @@ import (
 
 	"github.com/hdelazeri/lets-go-greenlight/internal/data"
 	"github.com/hdelazeri/lets-go-greenlight/internal/mailer"
+	"github.com/hdelazeri/lets-go-greenlight/internal/vcs"
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var version = vcs.Version()
 
 type config struct {
 	port int
@@ -79,7 +81,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version: \t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
