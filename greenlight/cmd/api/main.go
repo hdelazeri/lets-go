af839e901c290a9593fa82836e489e41445a2e19
Henrique Delazeri <hdelazeri@hdelazeri.dev>
2026-03-04 22:44:38 -0300

    [LGF] Chapter 8.3: Managing SQL query timeouts

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -20,10 +20,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn         string
-		maxOpenCons int
-		maxIdleCons int
-		maxIdleTime time.Duration
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
 	}
 }
 
@@ -40,8 +40,8 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-	flag.IntVar(&cfg.db.maxOpenCons, "db-max-open-cons", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleCons, "db-max-idle-cons", 25, "PostgreSQL max idle connections")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 
 	flag.Parse()
@@ -85,8 +85,8 @@ func openDB(cfg config) (*sql.DB, error) {
 		return nil, err
 	}
 
-	db.SetMaxOpenConns(cfg.db.maxOpenCons)
-	db.SetMaxIdleConns(cfg.db.maxIdleCons)
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
